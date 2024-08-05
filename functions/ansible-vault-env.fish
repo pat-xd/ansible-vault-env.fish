23a4a3b9b4d37cc23bf81a978dc28b640ab1a45d
pat <patrick.lorenz@ifam.fraunhofer.de>
2024-08-05 14:13:23 +0200

    [functions] removed init var-func again

--- functions/ansible-vault-env.fish
+++ functions/ansible-vault-env.fish
@@ -1,20 +1,17 @@
-function __activate_vault_init_vars
-    if set -q CREATE_RANDOM_DEFAULT
-        set CREATE_RANDOM_DEFAULT 24
-    end
-    if set -q VAULT_FILE_NAME
-        set VAULT_FILE_NAME .vault
-    end
-    if set -q VAULT_PASSKEY_FILE_NAME
-        set VAULT_PASSKEY_FILE_NAME .vault-key
-    end
-    if set -q ENV_FILE
-        set ENV_FILE .env
-    end
+if set -q CREATE_RANDOM_DEFAULT
+    set CREATE_RANDOM_DEFAULT 24
+end
+if set -q VAULT_FILE_NAME
+    set VAULT_FILE_NAME .vault
+end
+if set -q VAULT_PASSKEY_FILE_NAME
+    set VAULT_PASSKEY_FILE_NAME .vault-key
+end
+if set -q ENV_FILE
+    set ENV_FILE .env
 end
 
 function auto_activate_vault --on-variable PWD -d "auto-activate env from ansible-vault"
-    __activate_vault_init_vars
     if test -f $VAULT_FILE_NAME -a -f $VAULT_PASSKEY_FILE_NAME
         for line in (ansible-vault view --vault-password-file $VAULT_PASSKEY_FILE_NAME $VAULT_FILE_NAME | string split '\n')
                # Skip empty lines
@@ -33,7 +30,6 @@ function auto_activate_vault --on-variable PWD -d "auto-activate env from ansibl
 end
 
 function create_random -d"create a random string"
-    __activate_vault_init_vars
     set -l len $CREATE_RANDOM_DEFAULT
     if count $argv > 0
         set len $argv[1]
@@ -43,7 +39,6 @@ function create_random -d"create a random string"
 end
 
 function vault -d "create, view, load or edit an ansible-vault"
-    __activate_vault_init_vars
     switch $argv[1]
         case "create"
             if test -f $VAULT_FILE_NAME
@@ -65,7 +60,6 @@ function vault -d "create, view, load or edit an ansible-vault"
 end
 
 function auto_activate_env --on-variable PWD -d "auto-activate env if exists on change-directory"
-    __activate_vault_init_vars
     if test -f .env
         source .env
     end
